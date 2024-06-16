f99dbd35d93b540d5e079b069b2a88b440103443
86131 <1747675890@qq.com>
2024-06-16 09:13:05 +0800

    基础框架

---
+++ src/main/java/reviewSystem/ReviewSystemApplication.java
@@ -0,0 +1,13 @@
+package reviewSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/reviewSystem/ReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package reviewSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
